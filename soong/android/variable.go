8302f6e3274bb81ef1253640cfa952f8eefba44a
Christian Oder <myself5@carbonrom.org>
2017-11-24 15:14:20 +0100

    vendor: Add TARGET_OMX_LEGACY_RESCALING flag
    Change-Id: Ib755dda84a9d5a1994da2dccdb3571c45c95b6ce

--- soong/android/variable.go
+++ soong/android/variable.go
@@ -19,6 +19,10 @@ type Product_variables struct {
 	Qcom_bsp_legacy struct {
 		Cflags []string
 	}
+
+	Use_legacy_rescaling struct {
+		Cflags []string
+	}
 }
 
 type ProductVariables struct {
@@ -30,4 +34,5 @@ type ProductVariables struct {
 	QTIMediaPath            *string `json:",omitempty"`
 	Cant_reallocate_omx_buffers *bool `json:",omitempty"`
 	Qcom_bsp_legacy         *bool `json:",omitempty"`
+	Use_legacy_rescaling    *bool `json:",omitempty"`
 }
